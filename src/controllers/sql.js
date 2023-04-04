0279f9c00ebbf90841bddbeb03d6a8ef625274ed
ruben-isiTech <118488896+rdcamacho-isi@users.noreply.github.com>
2023-04-04 05:54:28 -0500

    Funcion para escapar caracteres especiales

--- src/controllers/sql.js
+++ src/controllers/sql.js
@@ -232,6 +232,29 @@ const bdConectionMasiveSqlInsert = async (
 };
 
 
+const escapeSqlString = (str) => {
+    return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+      switch (char) {
+        case "\0":
+          return "\\0";
+        case "\x08":
+          return "\\b";
+        case "\x09":
+          return "\\t";
+        case "\x1a":
+          return "\\z";
+        case "\n":
+          return "\\n";
+        case "\r":
+          return "\\r";
+        case "\"":
+        case "'":
+        case "\\":
+        case "%":
+          return "\\" + char; // Escape single quote, double quote, backslash and percent sign
+      }
+    });
+  }
 
 
-export { bdConnection, bdConectionMasiveSqlInsert };
+export { bdConnection, bdConectionMasiveSqlInsert, escapeSqlString };
