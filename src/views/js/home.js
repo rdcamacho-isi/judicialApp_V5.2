24c2d13876e0e2aa4e400aad226dc3a8fe230dce
ruben-isi <rdcamacho@soyisi.co>
2023-04-09 10:53:03 -0500

    actualizarComentariosLogin

--- src/views/js/home.js
+++ src/views/js/home.js
@@ -1,5 +1,7 @@
+// En caso de que no se vaya a usar el login no se debe usar la siguiente linea
+// import login from "./login.js"; 
+
 import * as utilidades from "./utilidades.js";
-import login from "./login.js"; 
 
 (()=>{
     'use strict'
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -6,14 +6,22 @@ import * as utilidades from "./utilidades.js";
 
     window.onload = () => {
  
-        if (sessionStorage.getItem("sesion") === null) {
-            utilidades.loadingStart();
-            window.location.href = "./login";
-            utilidades.loadingEnd();
-        } else {
-            utilidades.loadingStart();
-            window.location.href = "./home"; 
-            utilidades.loadingEnd();
-        }
+        // En caso de que se vaya a usar el login se debe INACTIVAR la condicion No2 y ACTIVAR la condicion No1
+        
+        // condicion No1
+        // if (sessionStorage.getItem("sesion") === null) {
+        //     utilidades.loadingStart();
+        //     window.location.href = "./login";
+        //     utilidades.loadingEnd();
+        // } else {
+        //     utilidades.loadingStart();
+        //     window.location.href = "./home"; 
+        //     utilidades.loadingEnd();
+        // }
+
+        // condicion No2
+        utilidades.loadingStart();
+        window.location.href = "./home"; 
+        utilidades.loadingEnd();        
     }
 })()
